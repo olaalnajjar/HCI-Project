4af1e94ce75f328c4416689289b34b6b2f95d270
Ingy-Mysara <67437146+Ingy-Mysara@users.noreply.github.com>
2023-05-08 02:12:48 +0300

    Restaurant menu and delivery options HTML done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RestaurantManagerComponent } from './restaurant-manager/restaurant-manager.component';
+import { RestaurantManager2Component } from './restaurant-manager2/restaurant-manager2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RestaurantManagerComponent,
+    RestaurantManager2Component
   ],
   imports: [
     BrowserModule,

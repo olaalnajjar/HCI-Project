bdd6029471c2f213a276d0caa25d29a5b8e89bdb
MadonnaBassem99 <70482229+MadonnaBassem99@users.noreply.github.com>
2023-05-18 10:27:29 +0300

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StorePageComponent } from './store-page/store-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 const routes: Routes = [
   {path:'',component: HomeComponent},
   {path: 'store/:id', component:StorePageComponent},
-  {path:'cart-page', component: CartPageComponent}
+  {path:'cart-page', component: CartPageComponent},
+  {path: 'logout', component: LoginComponent},
+  {path: 'new-account', component: SignUpComponent},
+  {path: 'login', component:LoginComponent}
 ];
 
 @NgModule({
